59153086959c52ae165f4b45e42764afebfc4dd4
Jonathan R Soulen <jacksoulen@gmail.com>
2024-12-05 11:43:17 -0500

    running ef commands. I'll probably clean this up at the end so it'll look clean

---
+++ EdgarRepo/Migrations/20241205163955_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EdgarRepo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EdgarCompanies",
+                columns: table => new
+                {
+                    Cik = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EntityName = table.Column<string>(type: "TEXT", nullable: false),
+                    Frame = table.Column<string>(type: "TEXT", nullable: false),
+                    Value = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EdgarCompanies", x => x.Cik);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EdgarCompanies");
+        }
+    }
+}
