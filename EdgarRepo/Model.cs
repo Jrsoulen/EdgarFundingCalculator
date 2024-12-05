c3400e2bd1ac6a4a3d8c755cf81aa4e5c285d010
Jonathan R Soulen <jacksoulen@gmail.com>
2024-12-05 11:42:25 -0500

    adding second c# project so i can show off some dependency injection later

---
+++ EdgarRepo/Model.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+public class EdgarContext : DbContext
+{
+    public DbSet<EdgarCompanyInfo> EdgarCompanies { get; set; }
+
+    public string DbPath { get; }
+
+    public EdgarContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, "edgar.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class EdgarCompanyInfo
+{
+    [Key]
+    public int Cik { get; set; }
+    public required string EntityName { get; set; }
+    public required string Frame { get; set; }
+    public required string Value { get; set; }
+}
