aba7180c0501abc08a60958489c5c4b419f00e3f
Jonathan R Soulen <jacksoulen@gmail.com>
2024-12-05 11:55:15 -0500

    Add sample calls to work from

--- EdgarRepo/Program.cs
+++ EdgarRepo/Program.cs
@@ -1,6 +1,31 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
+using System.Reflection.Metadata;
 
-app.MapGet("/", () => "Hello World!");
+using var db = new EdgarContext();
 
-app.Run();
+// Note: This sample requires the database to be created before running.
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+// Create
+Console.WriteLine("Inserting a new blog");
+db.Add(new EdgarCompanyInfo { Cik = 4,
+    EntityName = "Testing",
+    Frame = "",
+    Value = "Test Value"});
+db.SaveChanges();
+
+// Read
+Console.WriteLine("Querying for a blog");
+var blog = db.EdgarCompanies
+    .OrderBy(b => b.Cik)
+    .First();
+Console.WriteLine(blog.Value);
+
+// Delete
+Console.WriteLine("Delete the blog");
+db.Remove(blog);
+db.SaveChanges();
+
+Console.ReadLine();
