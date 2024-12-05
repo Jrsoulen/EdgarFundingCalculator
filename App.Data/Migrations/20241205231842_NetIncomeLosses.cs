7c30831973a20f98cc6983297d4b93eba03159a5
Jonathan R Soulen <jacksoulen@gmail.com>
2024-12-05 18:50:07 -0500

    migrations

---
+++ App.Data/Migrations/20241205231842_NetIncomeLosses.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EdgarRepo.Migrations
+{
+    /// <inheritdoc />
+    public partial class NetIncomeLosses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Frame",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Value",
+                table: "Companies");
+
+            migrationBuilder.CreateTable(
+                name: "YearNetIncomeLoss",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Frame = table.Column<string>(type: "TEXT", nullable: false),
+                    Year = table.Column<int>(type: "INTEGER", nullable: false),
+                    Value = table.Column<decimal>(type: "TEXT", nullable: false),
+                    CompanyCik = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_YearNetIncomeLoss", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_YearNetIncomeLoss_Companies_CompanyCik",
+                        column: x => x.CompanyCik,
+                        principalTable: "Companies",
+                        principalColumn: "Cik");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_YearNetIncomeLoss_CompanyCik",
+                table: "YearNetIncomeLoss",
+                column: "CompanyCik");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "YearNetIncomeLoss");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Frame",
+                table: "Companies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Value",
+                table: "Companies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
