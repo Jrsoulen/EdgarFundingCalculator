dbe98a65ecfa6ba412c9916deb6c97294ccf64d0
Jonathan R Soulen <jacksoulen@gmail.com>
2024-12-05 13:51:09 -0500

    Changes to start di

--- EdgarRepo/Model.cs
+++ EdgarRepo/Model.cs
@@ -5,19 +5,9 @@ public class EdgarContext : DbContext
 {
     public DbSet<EdgarCompanyInfo> EdgarCompanies { get; set; }
 
-    public string DbPath { get; }
-
-    public EdgarContext()
+    public EdgarContext(DbContextOptions<EdgarContext> options) : base(options)
     {
-        var folder = Environment.SpecialFolder.LocalApplicationData;
-        var path = Environment.GetFolderPath(folder);
-        DbPath = Path.Join(path, "edgar.db");
     }
-
-    // The following configures EF to create a Sqlite database file in the
-    // special "local" folder for your platform.
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite($"Data Source={DbPath}");
 }
 
 public class EdgarCompanyInfo
