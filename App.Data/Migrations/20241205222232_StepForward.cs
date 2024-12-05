272fb1338aa4c28f7ef36a80696bbaf82902e4dd
Jonathan R Soulen <jacksoulen@gmail.com>
2024-12-05 17:41:23 -0500

    migrations

---
+++ App.Data/Migrations/20241205222232_StepForward.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EdgarRepo.Migrations
+{
+    /// <inheritdoc />
+    public partial class StepForward : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EdgarCompanies");
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Cik = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EntityName = table.Column<string>(type: "TEXT", nullable: false),
+                    Frame = table.Column<string>(type: "TEXT", nullable: false),
+                    Value = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Cik);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.CreateTable(
+                name: "EdgarCompanies",
+                columns: table => new
+                {
+                    Cik = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EntityName = table.Column<string>(type: "TEXT", nullable: false),
+                    Frame = table.Column<string>(type: "TEXT", nullable: false),
+                    Value = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EdgarCompanies", x => x.Cik);
+                });
+        }
+    }
+}
