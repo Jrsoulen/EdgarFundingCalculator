74244093bf6ff6d38a9dc56d3dd85c5e6a35b62a
Jonathan R Soulen <jacksoulen@gmail.com>
2024-12-05 15:42:40 -0500

    business logic service

---
+++ EdgarFundingCalculator/EdgarFundingCalculatorService.cs
@@ -0,0 +1,45 @@
+﻿using App.Data;
+
+namespace App.Web
+{
+    /// <summary>
+    /// All Business Logic for application is here
+    /// </summary>
+    public class EdgarFundingCalculatorService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ICompanyRepository _companyRepo;
+        public EdgarFundingCalculatorService(HttpClient httpClient, ICompanyRepository companyRepo)
+        {
+            _httpClient = httpClient;
+            _companyRepo = companyRepo;
+        }
+
+        public async Task PopulateCompanyData(List<int> ciks)
+        {
+            foreach (var cik in ciks)
+            {
+                var foraData = _companyRepo.GetEdgarCompanyInfo(cik);
+                if (foraData != null) continue; // Already populated, do we care?
+
+                var edgarResponse = await GetCompanyFacts(cik);
+
+                _companyRepo.CreateEdgarCompanyInfo(edgarResponse.MapToCore());
+            }
+        }
+
+        public async Task<EdgarCompanyFactsResponse> GetCompanyFacts(int cik)
+        {
+            // API wants leading zeros on 10 digit CIK
+            var cikString = cik.ToString();
+            while (cikString.Length < 10) cikString = "0" + cikString;
+
+            var companyFacts = await _httpClient.GetFromJsonAsync<EdgarCompanyFactsResponse>($"api/xbrl/companyfacts/CIK{cikString}.json");
+            if (companyFacts == null)
+                throw new ArgumentException($"CIK ${cik} does not exist.");
+
+            return companyFacts;
+        }
+    }
+
+}
