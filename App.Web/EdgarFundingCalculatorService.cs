654cbfbc44c8a11a1547441ae46e0e50be826c4a
Jonathan R Soulen <jacksoulen@gmail.com>
2024-12-07 11:35:06 -0500

    Adding calculations

--- App.Web/EdgarFundingCalculatorService.cs
+++ App.Web/EdgarFundingCalculatorService.cs
@@ -1,6 +1,5 @@
 ﻿using App.Data;
 
-
 namespace App.Web
 {
 
@@ -67,28 +66,55 @@ namespace App.Web
         private decimal CalculateStandardFundableAmount(Company company)
         {
             // Company must have income data for all years between (and including) 2018 and 2022.
-            var requiredYears = new int[2018, 2019, 2020, 2021, 2022];
+            var requiredYears = new List<int>() { 2018, 2019, 2020, 2021, 2022 };
+            decimal maxRequiredYearValue = 0;
             foreach (var requiredYear in requiredYears)
             {
-                if (!company.YearlyNetIncomeLosses.Where(y => y.Year == requiredYear).Any()) return 0;
+                var requiredYearIncome = company.YearlyNetIncome.Where(y => y.Year == requiredYear).FirstOrDefault();
+                if (requiredYearIncome == null) return 0;
+                if (requiredYearIncome.Value > maxRequiredYearValue) maxRequiredYearValue = requiredYearIncome.Value;
             }
 
             // Company must have had positive income in both 2021 and 2022.
-            var positiveYears = new int[2021, 2022];
+            var positiveYears = new List<int>() { 2021, 2022 };
             foreach (var requiredYear in positiveYears)
             {
-                var yearData = company.YearlyNetIncomeLosses.Where(y => y.Year == requiredYear).First();
-                // Less than? Because these are income losses?
-                if (0 < yearData.Value)
+                var yearData = company.YearlyNetIncome.Where(y => y.Year == requiredYear).First();
+
+                if (yearData.Value <= 0)
                     return 0;
             }
 
-            return 1;
+            // Using highest income between 2018 and 2022
+            if (maxRequiredYearValue >= 10000000000)
+            {
+                // If income is greater than or equal to $10B, standard fundable amount is 12.33% of income
+                return maxRequiredYearValue * .1233m;
+            }
+            // If income is less than $10B, standard fundable amount is 21.51% of income
+            return maxRequiredYearValue * .2151m;
         }
 
         private decimal CalculateSpecialFundableAmount(Company company)
         {
-            return 0;
+            var standardFundingAmount = CalculateStandardFundableAmount(company);
+            if (standardFundingAmount == 0) return 0;
+
+            // If the company name starts with a vowel, add 15% to the standard funding amount
+            var vowels = new List<char>() { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            // Sometimes Y?
+            if (new Random().NextDouble() >= 0.5) vowels.AddRange('y', 'Y');
+            decimal vowelBonus = 0;
+            if (vowels.Exists(c => c == company.EntityName[0])) vowelBonus = standardFundingAmount * .15m;
+
+
+            // If the company’s 2022 income was less than their 2021 income, subtract 25% from their standard funding amount
+            decimal incomePenalty = 0;
+            if (company.YearlyNetIncome.Single(i => i.Year == 2022).Value <
+                company.YearlyNetIncome.Single(i => i.Year == 2021).Value)
+                incomePenalty = standardFundingAmount * .25m;
+
+            return standardFundingAmount + vowelBonus - incomePenalty;
         }
     }
 }
